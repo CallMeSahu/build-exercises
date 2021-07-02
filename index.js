f274c6d23b7b50f61e2451df6ece12b549bbc335
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-07-02 18:18:18 +0000

    wip: oops

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
-//Sum of array
-function addArray(arr){
-  return arr.reduce((a, b) => a+b, 0);
-};
+const studentDetails = [
+	{
+		roll: "1",
+		name: "Rohan Singh",
+		maths: 86,
+		science: 90,
+		english: 75,
+		computer: 85
+	},
+	{
+		roll: "2",
+		name: "Ritvik Patel",
+		maths: 27,
+		science: 30,
+		english: 35,
+		computer: 30
+	},
+	{
+		roll: "3",
+		name: "Neha Maurya",
+		maths: 75,
+		science: 69,
+		english: 40,
+		computer: 75
+	},
+	{
+		roll: "4",
+		name: "Mohit Verma",
+		maths: 21,
+		science: 31,
+		english: 45,
+		computer: 40
+	},
+	{
+		roll: "5",
+		name: "Karan Trivedi",
+		maths: 70,
+		science: 80,
+		english: 35,
+		computer: 60
+	}
+];
+/* 
+//Print name and total marks
+for(var i=0; i<studentDetails.length; i++){
+  console.log("Name:", studentDetails[i].name);
+  var totalMarks = studentDetails[i].maths + studentDetails[i].science + studentDetails[i].english + studentDetails[i].computer;
+  console.log("Total marks:",totalMarks);
+  console.log("------------------");
+} 
 
-//Find avarage of an array
-function getArrayAverage(arr){
-  return (arr.reduce((a, b)=> a+b,0))/arr.length;
-};
-
-//Maximum and minimum of array
-function getMinMax(arr){
-  var min=arr[0], max=arr[0];
-  for(var i=0; i<arr.length; i++){
-    if(arr[i] >= max){
-      max = arr[i];
-    }
-    else if(arr[i]<min){
-      min = arr[i];
-    }
-  }
-  console.log("Maximum:",max);
-  console.log("Minimum:",min);
+// Total marks is highest 
+var toppersMarks = studentDetails[0].maths + studentDetails[0].science + studentDetails[0].english + studentDetails[0].computer;
+var toppersName = studentDetails[0].name;
+for(var i=0; i<studentDetails.length; i++){
+  var totalMarks = studentDetails[i].maths + studentDetails[i].science + studentDetails[i].english + studentDetails[i].computer;
+  if(totalMarks > toppersMarks){
+    toppersMarks = totalMarks;
+    toppersName = studentDetails[i].name;
+  }    
 }
+console.log("Topper's Name:",toppersName);
+console.log("toppersMarks", toppersMarks); 
 
-//Find Median and Mode
-function getMedianMode(arr){
-  var median=0;
-  var i = Math.floor(arr.length / 2);
-  if(arr.length % 2 == 0){    
-    median = parseFloat((arr[i]+arr[i+1]))/2;
-  }
-  else{
-    median = (arr[i]);
-  }
-  console.log("Median:",median);
-  var mode = 0;
-  var maxCount=0;
-  for(var i=0; i<arr.length; i++){
-    var count = 0;
-    for(var j=0; j<arr.length; j++){
-      if(arr[i] == arr[j]){
-        count++;
-      }
-    }
-    if(count>maxCount){
-      maxCount = count;
-      mode = arr[i];
-    }
-  }
-  console.log("Mode:", mode);
-}
 
-// Sum of two array
-function addTwoArray(arr1, arr2){
-  var sum1=0, sum2=0;
-  for(var i=0; i<arr1.length; i++){
-    sum1 += parseInt(arr1[i]);
-  }
-  for(var i=0; i<arr2.length; i++){
-    sum2 += parseInt(arr2[i]);
-  }
-  return sum1+sum2;
+//Lowest Marks
+var lowestMarks = studentDetails[0].maths + studentDetails[0].science + studentDetails[0].english + studentDetails[0].computer;
+var lowestName = studentDetails[0].name;
+for(var i=0; i<studentDetails.length; i++){
+  var totalMarks = studentDetails[i].maths + studentDetails[i].science + studentDetails[i].english + studentDetails[i].computer;
+  if(totalMarks < lowestMarks){
+    lowestMarks = totalMarks;
+    lowestName = studentDetails[i].name;
+  }    
 }
+console.log("Lowest Scorer:",lowestName);
+console.log("Marks Scored", lowestMarks); 
+*/
 
-//Shift array by x
-function shiftArray(arr, x){
-  var noe = arr.length;
-    var newArray = [];
-  for(var i=noe-1; i<=noe-1-x; i--){
-    newArray.push(arr[i]);
-  }
-  for(var i=0; i<noe-1-x; i++){
-    newArray.push(arr[i]);
-  }
-  return newArray;
+//Average marks in computer science
+var totalMarks = 0;
+var totalstudents = 0;
+for(var i=0; i<studentDetails.length; i++){
+  totalMarks += studentDetails[i].computer;
+  totalstudents++;
 }
+var averageInCS = totalMarks/totalstudents;
+console.log("Average Marks in computers:", averageInCS);
+
+
+
